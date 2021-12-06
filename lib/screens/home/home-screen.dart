e1bcfae7271d2ebfb07cf5dcaa8b7598b1ba7f26
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-12-06 16:59:38 +0530

    first commit

---
+++ lib/screens/home/home-screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = '/home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
+  }
+}
