78b4801b37bc8eb2a74a6f4e703446a85b209b8d
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-12-09 21:29:21 +0530

    added UI elements

--- lib/screens/home/settings/settingspage.dart
+++ lib/screens/home/settings/settingspage.dart
@@ -1,12 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Settings'),
+    return ListView(children: <Widget>[
+      Container(
+        height: 100,
+        width: double.infinity,
+        child: Card(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Funds available to invest:"),
+              Text(
+                "₹ " + "19,999,000",
+                style: TextStyle(fontSize: 35),
+              ),
+            ],
+          ),
+        ),
       ),
-    );
+      ListTile(
+        title: Text('Reset portfolio'),
+        trailing: Icon(Icons.delete),
+      ),
+      ListTile(
+        title: Text('Rate the app'),
+        trailing: Icon(Icons.star),
+      ),
+      ListTile(
+        title: Text('Share the app'),
+        trailing: Icon(Icons.share),
+      ),
+      ListTile(
+        title: Text('About'),
+        trailing: Icon(Icons.info),
+      ),
+    ]);
   }
 }
